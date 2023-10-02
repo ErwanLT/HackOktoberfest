f5d87ed75e6fd6df19b604282ef13f8e31f3a38d
Erwan <erwanletutour.elt@gmail.com>
2023-10-02 08:31:55 +0200

    init project

---
+++ src/test/java/fr/eletutour/hackOktoberfest/HackOktoberfestApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eletutour.hackOktoberfest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackOktoberfestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
